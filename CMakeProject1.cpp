b5a66c18cd7f2e1592a31227f8a67efb6aea4a2e
hectorlin <silverlin2@msn.com>
2025-07-24 00:11:23 +0800

    Initial commit: CMake project files

---
+++ CMakeProject1.cpp
@@ -0,0 +1,13 @@
+﻿// CMakeProject1.cpp: 定義應用程式的進入點。
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	cout << "hey wilson" << endl;
+	return 0;
+}
